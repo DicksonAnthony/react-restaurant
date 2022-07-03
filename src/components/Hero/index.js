8a9b09a704a2f87718f916313efcb56efdae263e
DicksonAnthony <huntaba6@gmail.com>
2022-07-03 18:03:43 +0100

    this thing na Agbaka m isi oh 😒

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import Navbar from "../Navbar";
-import Sidebar from "../Sidebar";
+import React, { useState } from 'react';
+import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
 import {
   HeroContainer,
   HeroContent,
   HeroItems,
   HeroH1,
   HeroP,
-} from "./HeroElements";
+  HeroBtn
+} from './HeroElements';
 
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +16,7 @@ const Hero = () => {
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <HeroContainer>
       <Navbar toggle={toggle} />
@@ -23,6 +25,7 @@ const Hero = () => {
         <HeroItems>
           <HeroH1>Greatest Pizza Ever</HeroH1>
           <HeroP>Ready in 60 seconds</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
         </HeroItems>
       </HeroContent>
     </HeroContainer>
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,14 +1,15 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { FaPizzaSlice } from "react-icons/fa";
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+import { FaPizzaSlice } from 'react-icons/fa';
 
-export const Nav = styled.div`
+export const Nav = styled.nav`
   background: transparent;
   height: 80px;
   display: flex;
   justify-content: center;
   font-weight: 700;
 `;
+
 export const NavLink = styled(Link)`
   color: #fff;
   font-size: 2rem;
@@ -23,7 +24,8 @@ export const NavLink = styled(Link)`
     left: 25px;
   }
 `;
-export const NavIcons = styled.div`
+
+export const NavIcon = styled.div`
   display: block;
   position: absolute;
   top: 0;
@@ -36,6 +38,7 @@ export const NavIcons = styled.div`
     font-weight: bold;
   }
 `;
+
 export const Bars = styled(FaPizzaSlice)`
   font-size: 2rem;
   transform: translate(-50%, -15%);
